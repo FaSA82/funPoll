41e1be22380bbe5b09507186a6004ac78d47e884
Sergio <grey_f@inbox.ru>
2021-05-22 21:16:40 +0300

    Upgrated User model tests

---
+++ test/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:username) { |n| "user-#{n}" }
+    sequence(:email) { |n| "user-#{n}@email" }
+    password { '123456' }
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,28 +1,22 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  #test 'valid user' do
-  #  user = User.new(username: 'Vasya', email: 'pupkin@gmail.com', password: 'q1w2e3r4t5y6')
-  #  assert user.valid?
-  #end
+  test 'valid user' do
+    user = User.new(username: 'Vasya', email: 'pupkin@gmail.com', password: 'q1w2e3r4t5y6')
+    assert user.valid?
+  end
 
-  #test 'invalid without name' do
-  #  user = User.new(email: 'pupkin@gmail.com')
-  #  refute user.valid?, 'user is valid without a name'
-  #  assert_not_nil user.errors[:username], 'username validation does not work'
-  #end
 
   context "validations" do
-    #subject { FactoryBot.build(:user) }
-    User.new(email: 'pupkin@gmail.com')
+    subject { FactoryBot.build(:user) }
     should validate_presence_of(:username)
-    #should validate_presence_of(:email)
+    should validate_presence_of(:email)
+    should validate_uniqueness_of(:email)
+    should validate_uniqueness_of(:username)
+    should allow_value(/\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/).for(:email)
+    #  should_not allow_value('invalid.email').for(:email)
   end
   
 
-  #test 'invalid without email' do
-  #  user = User.new(username: 'John')
-  #  refute user.valid?, 'user is valid without an email'
-  #  assert_not_nil user.errors[:email], 'email validation does not work' 
-  #end
+
 end
